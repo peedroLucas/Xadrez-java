d66ac95a06254dbae2d96b2428b2f88a75e32354
pedro lucas <peedroo3133@outlook.com>
2022-11-03 14:37:45 -0300

    Primeira classe: posição

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import Tabluerio.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(""+pos);
+	}
+
+}
